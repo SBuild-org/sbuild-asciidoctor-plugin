72083246f2fe7117dba3a363bee96d67cbd8cd89
Tobias Roeser <le.petit.fou@web.de>
2014-01-30 11:10:48 +0100

    Started to document the plugin configuration class.

--- org.sbuild.plugins.asciidoctor/src/main/scala/org/sbuild/plugins/asciidoctor/Asciidoctor.scala
+++ org.sbuild.plugins.asciidoctor/src/main/scala/org/sbuild/plugins/asciidoctor/Asciidoctor.scala
@@ -3,6 +3,29 @@ package org.sbuild.plugins.asciidoctor
 import java.io.File
 import de.tototec.sbuild._
 
+/**
+ * Configuration for the SBuild Asciidoctor Plugin.
+ *
+ * Based on it's configuration, this plugin will register various targets.
+ * 
+ * Most configuration settings are directly submitted to the Asciidoctor generator. 
+ * Please refer to the Asciidoctor Manual and the Helf of the Asciidoctor Commandline Interface.
+ * 
+ * @param sourceFiles The AsciiDoc files.
+ * @param targetDir The destination directory for the generated files.
+ *   If `[[scala.None]]`, the files will be generated next to their sources.
+ * @param backend The backend used to generate the output. 
+ *   If `[[scala.None]]`, the default backend of Asciidoctor will be used (probably `html5`).
+ * @param baseDir 
+ * @param docType
+ * @param generateTargetName The Name of the SBuild `[[de.tototec.sbuild.Target Target]]` to invoke the Asciidoctor generator.
+ * @param attributes
+ * @param compactOutput
+ * @param safeMode
+ * @param sectionNumbers
+ * @param templateDir
+ * @param verbose
+ */
 case class Asciidoctor(
   sourceFiles: TargetRefs = Seq(),
   targetDir: Option[File] = None,
